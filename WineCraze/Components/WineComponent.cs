db651e19a2e5b878331cb562ea180295a67e4f5c
mrtn03 <mpetrov903@gmail.com>
2024-03-11 12:07:47 +0200

    add component to the project

---
+++ WineCraze/Components/WineComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WineCraze.Components
+{
+    public class WineComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
