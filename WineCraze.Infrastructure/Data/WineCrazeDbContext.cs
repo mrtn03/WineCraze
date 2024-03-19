109ad0c533fb7c55d8d8ad07b112a957439cf5ab
mrtn03 <mpetrov903@gmail.com>
2024-03-19 16:47:04 +0200

    Add Fluent-Api

--- WineCraze.Infrastructure/Data/WineCrazeDbContext.cs
+++ WineCraze.Infrastructure/Data/WineCrazeDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 using WineCraze.Infrastructure.Data.Models;
 
 namespace WineCraze.Data
@@ -17,9 +18,64 @@ namespace WineCraze.Data
         public DbSet<Supplier> Suppliers { get; set; } = null!;
         public DbSet<Report> Reports { get; set; } = null!;
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Sale>()
+               .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales)
+                .HasForeignKey(s => s.CustomerId)
+                .OnDelete(DeleteBehavior.NoAction); 
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Supplier)
+                .WithMany(su => su.Sales)
+                .HasForeignKey(s => s.SupplierId)
+                .OnDelete(DeleteBehavior.NoAction); 
+
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.Report)
+               .WithMany(r => r.Sales)
+                .HasForeignKey(s => s.ReportId)
+                .OnDelete(DeleteBehavior.NoAction);
+            
+
+            //modelBuilder.Entity<Sale>()
+            //    .HasOne(s => s.Customer)
+            //    .WithMany(c => c.Sales)
+            //    .HasForeignKey(s => s.CustomerId);
+
+            //modelBuilder.Entity<Sale>()
+            //    .HasOne(s => s.Supplier)
+            //    .WithMany(su => su.Sales)
+            //    .HasForeignKey(s => s.SupplierId);
+
+            //modelBuilder.Entity<Sale>()
+            //    .HasOne(s => s.Report)
+            //    .WithMany(r => r.Sales)
+            //    .HasForeignKey<Report>(r => r.SaleId);
+
+            //modelBuilder.Entity<Sale>()
+            //    .HasOne(s => s.Wine)
+            //    .WithMany(w => w.Sales)
+            //    .HasForeignKey(s => s.WineId);
+
+            //modelBuilder.Entity<Customer>()
+            //    .HasMany(c => c.Sales)
+            //    .WithOne(s => s.Customer)
+            //    .HasForeignKey(s => s.CustomerId);
+
+            //modelBuilder.Entity<Supplier>()
+            //    .HasMany(su => su.Sales)
+            //    .WithOne(s => s.Supplier)
+            //    .HasForeignKey(s => s.SupplierId);
+
+            //modelBuilder.Entity<Wine>()
+            //    .HasMany(w => w.Sales)
+            //    .WithOne(s => s.Wine)
+            //    .HasForeignKey(s => s.WineId);
+
         }
     }
 }
---
+++ WineCraze.Infrastructure/Migrations/20240319144246_UpdatedTable.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WineCraze.Infrastructure.Migrations
+{
+    public partial class UpdatedTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wines_Customers_CustomerId",
+                table: "Wines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Wines_CustomerId",
+                table: "Wines");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Wines");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Bulstat",
+                table: "Suppliers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Bulstat For Supplier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bulstat",
+                table: "Suppliers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Wines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Customer Identification");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wines_CustomerId",
+                table: "Wines",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wines_Customers_CustomerId",
+                table: "Wines",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
