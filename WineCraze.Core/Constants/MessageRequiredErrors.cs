70ebbba20ec52b2b491db344a7612fd97599817c
mrtn03 <mpetrov903@gmail.com>
2024-03-29 13:29:03 +0200

    add constants

---
+++ WineCraze.Core/Constants/MessageRequiredErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace WineCraze.Core.Constants
+{
+    public static class MessageRequiredErrors
+    {
+        public const string RequiredMessage = "The {0} field is required";
+
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone number already exists. Enter another one";
+    }
+}
