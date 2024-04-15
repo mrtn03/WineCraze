545b5d9bb7dcb9676af0a3ff3dce164fcf75a74d
mrtn03 <mpetrov903@gmail.com>
2024-04-15 18:27:20 +0300

    changes added

--- WineCraze.Core/Services/SupplierService.cs
+++ WineCraze.Core/Services/SupplierService.cs
@@ -23,6 +23,10 @@ namespace WineCraze.Core.Services
                     Id = s.Id,
                     Name = s.Name,
                     Address = s.Address,
+                    ContactPerson = s.ContactPerson,
+                    Email = s.Email,
+                    Phone = s.Phone,
+                    Bulstat = s.Bulstat
                 })
                 .ToList();
         }
@@ -40,19 +44,28 @@ namespace WineCraze.Core.Services
                 Id = supplier.Id,
                 Name = supplier.Name,
                 Address = supplier.Address,
+                ContactPerson = supplier.ContactPerson,
+                Bulstat = supplier.Bulstat,
+                Email = supplier.Email,
+                Phone = supplier.Phone
             };
         }
 
-        public void CreateSupplier(SupplierViewModel viewModel)
+        public async void CreateSupplier(SupplierViewModel viewModel)
         {
             var supplier = new Supplier
             {
                 Name = viewModel.Name,
                 Address = viewModel.Address,
+                Id = viewModel.Id,
+                ContactPerson = viewModel.ContactPerson,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Bulstat = viewModel.Bulstat
             };
 
-            _supp.AddAsync(supplier).Wait();
-            _supp.SaveChangesAsync().Wait();
+            await _supp.AddAsync(supplier);
+            await _supp.SaveChangesAsync();
         }
 
         public void UpdateSupplier(SupplierViewModel viewModel)
@@ -65,6 +78,10 @@ namespace WineCraze.Core.Services
 
             supplier.Name = viewModel.Name;
             supplier.Address = viewModel.Address;
+            supplier.Phone = viewModel.Phone;
+            supplier.ContactPerson = viewModel.ContactPerson;
+            supplier.Bulstat = viewModel.Bulstat;
+            supplier.Email = viewModel.Email;
 
             _supp.SaveChangesAsync().Wait();
         }
